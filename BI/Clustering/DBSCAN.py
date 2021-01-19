62d30690f8a17860c58ad2d4b813083a0a52c626
Sven_Gehrke <sven.gehrke@uni-jena.de>
2021-01-19 16:46:56 +0100

    update

--- BI/Clustering/DBSCAN.py
+++ BI/Clustering/DBSCAN.py
@@ -1,20 +1,19 @@
 # based on https://scikit-learn.org/stable/auto_examples/cluster/plot_dbscan.html#sphx-glr-auto-examples-cluster-plot-dbscan-py
-import numpy as np
+import itertools
 
-from sklearn.cluster import DBSCAN
+import numpy as np
 from sklearn import metrics
-from sklearn.datasets import make_blobs
+from sklearn.cluster import DBSCAN
 from sklearn.datasets import load_iris
 from sklearn.preprocessing import StandardScaler
 
-
 # #############################################################################
-# Generate sample data
-# centers = [[1, 1], [-1, -1], [1, -1]]
-# X, labels_true = make_blobs(n_samples=750, centers=centers, cluster_std=0.4, random_state=0)
+iris = load_iris()
+X = iris.data
+target = iris.target
+feature_names = iris.feature_names
 
-X,labels_true=load_iris(return_X_y=True)
-
+n_dim = X.shape[1]
 
 # scale the data
 X = StandardScaler().fit_transform(X)
@@ -32,13 +31,13 @@ n_noise_ = list(labels).count(-1)
 
 print('Estimated number of clusters: %d' % n_clusters_)
 print('Estimated number of noise points: %d' % n_noise_)
-print("Homogeneity: %0.3f" % metrics.homogeneity_score(labels_true, labels))
-print("Completeness: %0.3f" % metrics.completeness_score(labels_true, labels))
-print("V-measure: %0.3f" % metrics.v_measure_score(labels_true, labels))
+print("Homogeneity: %0.3f" % metrics.homogeneity_score(target, labels))
+print("Completeness: %0.3f" % metrics.completeness_score(target, labels))
+print("V-measure: %0.3f" % metrics.v_measure_score(target, labels))
 print("Adjusted Rand Index: %0.3f"
-	  % metrics.adjusted_rand_score(labels_true, labels))
+	  % metrics.adjusted_rand_score(target, labels))
 print("Adjusted Mutual Information: %0.3f"
-	  % metrics.adjusted_mutual_info_score(labels_true, labels))
+	  % metrics.adjusted_mutual_info_score(target, labels))
 print("Silhouette Coefficient: %0.3f"
 	  % metrics.silhouette_score(X, labels))
 
@@ -50,22 +49,33 @@ import matplotlib.pyplot as plt
 unique_labels = set(labels)
 colors = [plt.cm.Spectral(each)
 		  for each in np.linspace(0, 1, len(unique_labels))]
-for k, col in zip(unique_labels, colors):
-	if k == -1:
-		# Black used for noise.
-		col = [0, 0, 0, 1]
+fig, ax = plt.subplots(n_dim, n_dim)
 
-	class_member_mask = (labels == k)
+l_dim=np.arange(0,n_dim)
+views = list(itertools.product(l_dim,l_dim))
 
-	xy = X[class_member_mask & core_samples_mask]
-	# plt.plot(xy[:, 0], xy[:, 1], 'o', markerfacecolor=tuple(col),
-	# 		 markeredgecolor='k', markersize=14)
-	plt.plot(xy[:, 0], xy[:, 1], 'o',
-			 markersize=4)
+for view in views:
+	for k, col in zip(unique_labels, colors):
+		if k == -1:
+			# Black used for noise.
+			col = [0, 0, 0, 1]
 
-	xy = X[class_member_mask & ~core_samples_mask]
-	plt.plot(xy[:, 0], xy[:, 1], '*', markerfacecolor=tuple(col),
-			 markeredgecolor='k', markersize=2)
+		class_member_mask = (labels == k)
 
-plt.title('Estimated number of clusters: {}'.format(n_clusters_))
+		xy = X[class_member_mask & core_samples_mask]
+		# plt.plot(xy[:, 0], xy[:, 1], 'o', markerfacecolor=tuple(col),
+		# 		 markeredgecolor='k', markersize=14)
+		ax[view[0], view[1]].plot(xy[:, view[0]], xy[:, view[1]], 'o', markersize=3)
+
+		xy = X[class_member_mask & ~core_samples_mask]
+		ax[view[0], view[1]].plot(xy[:, view[0]], xy[:, view[1]], '*', markerfacecolor=tuple(col),
+				markeredgecolor='k', markersize=1)
+
+
+	ax[view[0], view[1]].set_ylabel(feature_names[view[0]], fontsize=4)
+	ax[view[0], view[1]].set_xlabel(feature_names[view[1]], fontsize=4)
+	ax[view[0], view[1]].tick_params(axis="both", labelsize=4)
+
+plt.suptitle('Estimated number of clusters: {}'.format(n_clusters_))
 plt.show()
+fig.savefig('dbscan.jpeg', dpi=600 )
